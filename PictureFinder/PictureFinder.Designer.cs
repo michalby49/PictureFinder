5f479a24b3bfe3dc88483ec34c25cc46ea033958
michalby49 <beskamichal49@gmail.com>
2020-12-17 19:21:05 +0100

    Dodaj pliki projektów.

---
+++ PictureFinder/PictureFinder.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace PictureFinder
+{
+    partial class PictureFinder
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(12, 12);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "Przeglądaj";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(93, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Usuń";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbImage.Location = new System.Drawing.Point(12, 41);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(776, 397);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 2;
+            this.pbImage.TabStop = false;
+            // 
+            // PictureFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnBrowser);
+            this.Name = "PictureFinder";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PictureFinder_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.PictureBox pbImage;
+    }
+}
+
---
+++ PictureFinder/PictureFinder.cs
@@ -0,0 +1,80 @@
+﻿using PictureFinder.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureFinder
+{
+    public partial class PictureFinder : Form
+    {
+        private OpenFileDialog openFileDialog = new OpenFileDialog();      
+        public string ImagePath
+        {
+            get
+            {
+                return Settings.Default.ImagePath; 
+            }
+            set
+            {
+                Settings.Default.ImagePath = value;
+            }
+        }
+        public PictureFinder()
+        {
+            InitializeComponent();
+
+            openFileDialog.FileName = ImagePath;
+            PictureBoxCheck();
+
+            CheckDelete();
+        }
+        private void PictureBoxCheck()
+        {
+            if (ImagePath != null)
+            {
+                Bitmap bitmap = new Bitmap(ImagePath);
+                pbImage.Image = bitmap;
+            }
+        }
+        private void CheckDelete()
+        {
+            if (pbImage.Image == null)
+                btnDelete.Visible = false;
+            else
+                btnDelete.Visible = true;
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bitmap = new Bitmap(openFileDialog.FileName);
+                pbImage.Image = bitmap;  
+            }
+            
+            CheckDelete();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            pbImage.Image = null;
+            CheckDelete();
+        }
+
+        private void PictureFinder_FormClosed(object sender, FormClosedEventArgs e)
+        {  
+            if (pbImage != null)
+                ImagePath = openFileDialog.FileName;
+            else
+                ImagePath = null;
+
+            Settings.Default.Save();
+        }
+    }
+}
